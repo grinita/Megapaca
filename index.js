1521ac24847c7a816bd13e6ecea8d768ace78cc7
Grinita <130435308+grinita@users.noreply.github.com>
2023-04-15 14:41:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 const { Telegraf } = require("telegraf");
 
-const TOKEN = '';
+const TOKEN = con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM constantes", function (err, result, fields) {
+        if (err) throw err;
+        return result[0]["token"];
+    });
+});;
 
 
 const bot = new Telegraf(TOKEN)
@@ -12,8 +18,6 @@ bot.command('test', ctx => {
             reply_markup: {
                 inline_keyboard: [
                     [
-
-                        
                         { text: 'Rojo \u{1F534}', callback_data: 'red' },
                         { text: 'Verde \u{1F7E2}', callback_data: 'green' },
                         { text: 'Gris \u{1F518}', callback_data: 'gray' },
@@ -24,7 +28,7 @@ bot.command('test', ctx => {
                         { text: 'Celeste \u{1F48E}', callback_data: 'bblue' },
                     ],
                     [
-                        
+
                         { text: 'Amarillo \u{1F7E1}', callback_data: 'yellow' },
                         { text: 'Naranja \u{1F7E0}', callback_data: 'orange' },
                         { text: 'Blanco \u{26AA}', callback_data: 'white' },
@@ -42,7 +46,7 @@ bot.action('red', ctx => {
                 inline_keyboard: [
                     [
 
-                        
+
                         { text: 'Precio normal', callback_data: 'red' },
                         { text: '15%', callback_data: 'green' },
                     ],
@@ -52,7 +56,7 @@ bot.action('red', ctx => {
                         { text: '60%', callback_data: 'bblue' },
                     ],
                     [
-                        
+
                         { text: '70%', callback_data: 'yellow' },
                         { text: '80%', callback_data: 'orange' },
                         { text: '90%', callback_data: 'white' },
