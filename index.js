4353880c22da74b5e424246597e4ad4c0450ecab
Ingrid Pérez <3057369060301@ingenieria.usac.edu.gt>
2023-05-26 02:13:19 -0600

    duuuuh

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ function mostrarPregunta(ctx, pregunta) {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'Rojo \u{1F534}', callback_data: 'red' },
-                    { text: 'Verde \u{1F7E2}', callback_data: 'green' },
-                    { text: 'Gris \u{1F518}', callback_data: 'gray' },
+                    { text: 'Rojo \u{1F534}', callback_data: 'Rojo' },
+                    { text: 'Verde \u{1F7E2}', callback_data: 'Verde' },
+                    { text: 'Gris \u{1F518}', callback_data: 'Gris' },
                 ],
                 [
-                    { text: 'Negro \u{26AB}', callback_data: 'black' },
-                    { text: 'Azul \u{1F535}', callback_data: 'blue' },
-                    { text: 'Celeste \u{1F48E}', callback_data: 'bblue' },
+                    { text: 'Negro \u{26AB}', callback_data: 'Negro' },
+                    { text: 'Azul \u{1F535}', callback_data: 'Azul' },
+                    { text: 'Celeste \u{1F48E}', callback_data: 'Celeste' },
                 ],
                 [
-                    { text: 'Amarillo \u{1F7E1}', callback_data: 'yellow' },
-                    { text: 'Naranja \u{1F7E0}', callback_data: 'orange' },
-                    { text: 'Blanco \u{26AA}', callback_data: 'white' },
+                    { text: 'Amarillo \u{1F7E1}', callback_data: 'Amarillo' },
+                    { text: 'Naranja \u{1F7E0}', callback_data: 'Naranja' },
+                    { text: 'Blanco \u{26AA}', callback_data: 'Blanco' },
                 ]
             ]
         },
